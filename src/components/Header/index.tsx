01c98be803d662acfb0792b00a06d18112d5c960
Sóstenis Vinícius <Socratis__@hotmail.com>
2024-01-05 21:16:07 -0300

    Adding a modal of radix library

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
